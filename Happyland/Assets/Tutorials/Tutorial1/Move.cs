cd1186b0d45e85bff916826c5062ad7d55439373
Nadia <nadianadeem365@gmail.com>
2022-09-29 21:53:04 +0100

    Lab 2 auto pilot complete.
    Lab 2 complete to the auto pilot section. FSM section left to complete.

--- Happyland/Assets/Tutorials/Tutorial1/Move.cs
+++ Happyland/Assets/Tutorials/Tutorial1/Move.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-public class Move : MonoBehaviour {
+public class Move : MonoBehaviour{
 
     public float speed = 2.0f;
 
@@ -8,16 +8,16 @@ public class Move : MonoBehaviour {
 
     public Transform goal;
 
-    void Start() 
+    void Start()
     {
     }
 
-    private void Update() 
+    private void Update()
     {
         this.transform.LookAt(goal.position);
         Vector3 direction = goal.position - this.transform.position;
         Debug.DrawRay(this.transform.position, direction, Color.red);
-        if(direction.magnitude > 1.0f)
+        if (direction.magnitude > 1.0f)
         {
             this.transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
         }
--- Happyland/Assets/Tutorials/Tutorial2/Scripts/move_starter.cs
+++ Happyland/Assets/Tutorials/Tutorial2/Scripts/move_starter.cs
@@ -10,14 +10,28 @@ public class move_starter : MonoBehaviour{
     // Public GameObject to store the seekable object in
     public GameObject seek_me;
 
+    float autoRotSpeed = 0.1f;
+    float autoSpeed = 0.1f;
+
+    bool AutoPilotMode = false;
+
     Vector3 Cross(Vector3 v, Vector3 w)
     {
 
         Vector3 crossProd = new Vector3( 0, 0, 0);
 
-        // crossProd = <do computation here>
+        crossProd.x = v.y * w.z - v.z * w.y;
+        crossProd.y = v.z * w.x - v.x * w.z;
+        crossProd.z = v.x * w.y - v.y * w.x;
 
         return crossProd;
+    }
+
+    void AutoPilot()
+    {
+        float angleToTurn = CalculateAngle();
+        this.transform.Rotate(0, angleToTurn, 0);
+        this.transform.Translate(this.transform.forward * autoSpeed, Space.World);
     }
 
     // Calculate the vector to the seek object
@@ -33,10 +47,10 @@ public class move_starter : MonoBehaviour{
         float angle = 0.0f; // angle var to return
 
         // Calculate the dot product
-        // dot = <do calculation here>
+        dot = (tF.x * sD.x) + (tF.y * sD.y) + (tF.z * sD.z);
 
         // Calculate the angle between the two items - be careful it is in degs and not rads
-        // angle = <do calculation here>
+        angle = Mathf.Acos(dot / (tF.magnitude * sD.magnitude)) * Mathf.Rad2Deg;
 
         // Output the angles to the console - these should be the same
         Debug.Log("Angle: " + angle);
@@ -47,12 +61,19 @@ public class move_starter : MonoBehaviour{
         Debug.DrawRay(this.transform.position, tF * 10.0f, Color.green, 2.0f);
         // Draw a ray showing the vector to the saught object
         Debug.DrawRay(this.transform.position, sD, Color.red, 2.0f);
+        //Draw a ray of cross product
+        Debug.DrawRay(this.transform.position, Cross(tF, sD), Color.blue, 2.0f);
 
-        return angle;
+        if (Cross(tF, sD).y < 0.0f)
+        {
+            angle *= -1;
+        }
+
+        return angle * autoRotSpeed;
     }
 
     // Calculate the distance from the chibi to whatever it is finding
-    void CalculateDistance()
+    float CalculateDistance()
     {
 
         // Chibi position
@@ -62,8 +83,10 @@ public class move_starter : MonoBehaviour{
 
         float distance = 0.0f;
 
+        Vector3 goalLength = sP-tP;
+
         // Calculate the distance between the objects using pythagoras
-        // distance = <do calculation here>
+        distance = Mathf.Sqrt(Mathf.Pow(goalLength.x,2) + Mathf.Pow(goalLength.y, 2) + Mathf.Pow(goalLength.z, 2));
 
         // Calculate and compare your calculation with the distance using Unitys vector distance function
         float unityDistance = Vector3.Distance(tP, sP);
@@ -71,9 +94,11 @@ public class move_starter : MonoBehaviour{
         // Print out the two results to the console - they should be the same
         Debug.Log("Distance: " + distance);
         Debug.Log("Unity Distance: " + unityDistance);
+
+        return distance;
     }
 
-    void Update()
+    void FixedUpdate()
     {
         // Get the horizontal and vertical axis.
         // By default they are mapped to the arrow keys.
@@ -81,16 +106,20 @@ public class move_starter : MonoBehaviour{
         float translation = Input.GetAxis("Vertical") * speed;
         float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
 
-        // Make it move 10 meters per second instead of 10 meters per frame...
-        translation *= Time.deltaTime;
-        rotation *= Time.deltaTime;
+        if (!AutoPilotMode)
+        {
+            // Make it move 10 meters per second instead of 10 meters per frame...
+            translation *= Time.deltaTime;
+            rotation *= Time.deltaTime;
 
-        // Move translation along the object's z-axis
-        transform.Translate(0, 0, translation);
+            // Move translation along the object's z-axis
+            transform.Translate(0, 0, translation);
 
-        // Rotate around our y-axis
-        transform.Rotate(0, rotation, 0);
+            // Rotate around our y-axis
+            transform.Rotate(0, rotation, 0);
 
+        }
+        
         // Check for the spacebar being pressed
         if (Input.GetKeyDown(KeyCode.Space))
         {
@@ -104,9 +133,25 @@ public class move_starter : MonoBehaviour{
         // Check for the T key being pressed
         if (Input.GetKeyDown(KeyCode.T))
         {
-            // Call CalculateAngle method
-            float angle_to_turn = CalculateAngle();
-            this.transform.Rotate(0, angle_to_turn,0);
+            AutoPilotMode = !AutoPilotMode;
+        }
+
+        if (AutoPilotMode)
+        {
+            if (CalculateDistance() > 1.0f)
+            {
+                AutoPilot();
+
+                // Make it move 10 meters per second instead of 10 meters per frame...
+                translation *= Time.deltaTime;
+                rotation *= Time.deltaTime;
+
+                // Move translation along the object's z-axis
+                seek_me.transform.Translate(0, 0, translation);
+
+                // Rotate around our y-axis
+                seek_me.transform.Rotate(0, rotation, 0);
+            }
         }
     }
 }
