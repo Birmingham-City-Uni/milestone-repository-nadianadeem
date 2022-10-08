a4463688485426221779caaa8f6ee2a2ed7c4d92
Nadia <nadianadeem365@gmail.com>
2022-10-08 14:21:14 +0100

    Add MiniBomb Agent empty script.

---
+++ Happyland/Assets/Scripts/Agents/MiniBomb.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniBomb : Agent
+{
+    // Update is called once per frame
+    public override void Update()
+    {
+        /*Quick plan of how FSM should work.
+        If finished spawning -> Wander (Another FSM)
+        If agent hears or sees enemy -> Attack (move and explode)
+        Attack -> Die
+        */
+    }
+}
