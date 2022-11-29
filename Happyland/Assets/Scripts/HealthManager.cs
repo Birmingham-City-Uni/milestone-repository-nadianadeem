ec308904e8cb2b479c33fe9d03f507ece55673f3
Nadia <nadianadeem365@gmail.com>
2022-11-29 17:33:29 +0000

    Main menu created implemented and working. Scene is reloaded when player has no health. Fix #35

--- Happyland/Assets/Scripts/HealthManager.cs
+++ Happyland/Assets/Scripts/HealthManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HealthManager : MonoBehaviour
 {
@@ -25,7 +26,7 @@ public class HealthManager : MonoBehaviour
 
         if(!isEnemy && currentHealth <= 0)
         {
-            Destroy(gameObject);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
 
