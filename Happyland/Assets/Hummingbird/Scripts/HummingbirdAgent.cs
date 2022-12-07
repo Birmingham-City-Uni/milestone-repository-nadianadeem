5f0fab243647f2386df0f14d4c0c03cbacd87d8b
Nadia <nadianadeem365@gmail.com>
2022-12-07 10:39:50 +0000

    Completed Flower area script. Started on hummingbird.

--- Happyland/Assets/Hummingbird/Scripts/HummingbirdAgent.cs
+++ Happyland/Assets/Hummingbird/Scripts/HummingbirdAgent.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.MLAgents;
 using UnityEngine;
 
-public class HummingbirdAgent : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+/// <summary>
+/// A hummingbird ML agent
+/// </summary>
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public class HummingbirdAgent : Unity.MLAgents.Agent
+{
+    [Tooltip("Force to apply when moving")]
+    public float moveForce = 2f;
+
+    [Tooltip("Speed to pitch up or down")]
+    public float pitchSpeed = 100f;
+
+    [Tooltip("Speed to rotate around the up axis")]
+    public float yawSpeed = 100f;
+
+    [Tooltip("Transform at the tip pf the beak")]
+    public Transform beakTip;
+
+    [Tooltip("Whether this is training or gameplay mode")]
+    public bool trainingMode;
 }
